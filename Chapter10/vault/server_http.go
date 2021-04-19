a380e45ab0e0fdd5c197541710734dd46f99bc42
Slimo300 <sebastian.slimak707@gmail.com>
2021-04-19 11:10:46 +0200

    no ctx argument for go-kit NewServer

--- Chapter10/vault/server_http.go
+++ Chapter10/vault/server_http.go
@@ -3,21 +3,20 @@ package vault
 import (
 	"net/http"
 
-	httptransport "github.com/go-kit/transport/http"
+	httptransport "github.com/go-kit/kit/transport/http"
 	"golang.org/x/net/context"
 )
 
 func NewHTTPServer(ctx context.Context, endpoints Endpoints) http.Handler {
 	m := http.NewServeMux()
 
-	m.Handle("/hash", httptransport.NewServer(ctx,
+	m.Handle("/hash", httptransport.NewServer(
 		endpoints.HashEndpoint,
 		decodeHashRequest,
 		encodeResponse,
 	))
 
 	m.Handle("/validate", httptransport.NewServer(
-		ctx,
 		endpoints.ValidateEndpoint,
 		decodeValidateRequest,
 		encodeResponse,
